aff605125469ffb805c2ab8c0e43ab2747938310
Patrickvsz <contatospatrickifbb@gmail.com>
2025-10-07 22:41:33 -0300

    Add PWA manifest, icons and splash screens

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.evodental.app',
+  appName: 'Evo Dental',
+  webDir: 'out'
+};
+
+export default config;
